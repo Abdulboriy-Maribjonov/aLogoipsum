c1a0ee99cf9cb724394a2f1fd3c7eaf01f98a8c9
Abdulboriy-Maribjonov <abdulboriymaribjonov08@gmail.com>
2025-12-30 15:52:37 +0500

    Finally finish

--- script.js
+++ script.js
@@ -23,3 +23,34 @@ document.addEventListener("touchstart", (e)=>{
     list.classList.remove("active")
   }
 })
+
+const group = document.querySelector('.group');
+const cards = Array.from(group.children);
+
+
+cards.forEach(card => {
+  const clone = card.cloneNode(true);
+  group.appendChild(clone);
+});
+
+
+let originalWidth = 0;
+cards.forEach(card => {
+  originalWidth += card.offsetWidth;
+});
+
+let x = 0;
+const speed = 0.5;
+
+function animate() {
+  x -= speed;
+
+  if (Math.abs(x) >= originalWidth) {
+    x = 0;
+  }
+
+  group.style.transform = `translateX(${x}px)`;
+  requestAnimationFrame(animate);
+}
+
+animate();
